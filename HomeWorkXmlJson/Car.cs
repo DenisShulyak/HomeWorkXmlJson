80c5666a863d3aa576d554d17bf995ff88f73a8d
DenisShulyak <denshufly@mail.ru>
2019-03-27 20:53:41 +0600

    I did it

---
+++ HomeWorkXmlJson/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkXmlJson
+{
+    public class Car
+    {
+        public string TypeCar { get; set; }
+        public string Mark { get; set; }
+        public double Power { get; set; }
+        public string Color { get; set;}
+    }
+}
